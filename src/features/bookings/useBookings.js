4fb8c2b0e4d481e9b7b5cdcddc090f346884537d
Your Name <you@example.com>
2024-03-13 16:31:34 +0530

    ---sortBy

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -7,6 +7,8 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
+  // FLTER
+
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
@@ -14,13 +16,19 @@ export function useBookings() {
       : { field: "status", value: filterValue };
   // { field: "totalPrice", value: 5000, method: "gte" };
 
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
