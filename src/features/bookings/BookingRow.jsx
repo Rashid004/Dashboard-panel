d33c7246c89d20872ac1a1e94b31e986bccdeeb8
Your Name <you@example.com>
2024-03-13 13:19:32 +0530

    date

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -55,7 +55,7 @@ function BookingRow({
     "checked-in": "green",
     "checked-out": "silver",
   };
-
+  console.log(startDate, endDate, bookingId);
   return (
     <Table.Row>
       <Cabin>{cabinName}</Cabin>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,7 @@
 /** @format */
 
-import { differenceInDays, formatDistance, parseISO } from "date-fns";
-
-// import { formatDistance, parseISO } from "date-fns";
-// import { differenceInDays } from "date-fns/esm";
-// import { differenceInDays, formatDistance, parseISO } from "date-fns";
+import { formatDistance, parseISO } from "date-fns";
+import { differenceInDays } from "date-fns/esm";
 
 // We want to make this function work for both Date objects and strings (which come from Supabase)
 export const subtractDates = (dateStr1, dateStr2) =>
